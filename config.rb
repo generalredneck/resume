31f79ac7b95a665e77f67bc3663fc67ed8ed7880
Allan Chappell <generalredneck@gmail.com>
2015-11-03 21:55:41 -0600

    Getting ready to work on Print.

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 require 'compass/import-once/activate'
 require 'autoprefixer-rails'
-# Require any additional compass plugins here.
+require 'font-awesome-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -11,12 +11,15 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
+
+sass_options = {:unix_newlines => true}
 
 
 # If you prefer the indented syntax, you might want to regenerate this
